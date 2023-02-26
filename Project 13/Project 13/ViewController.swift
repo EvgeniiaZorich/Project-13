36edbcb419fb45c02063fae5dee539b1a424f036
Evgeniia Zorich <vasileva.evg.al@gmail.com>
2023-02-27 00:55:52 +0100

    feature day 54 new

--- Project 13/Project 13/ViewController.swift
+++ Project 13/Project 13/ViewController.swift
@@ -46,6 +46,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let beginImage = CIImage(image: currentImage)
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
         applyProcessing()
+        
+        imageView.alpha = 0
+        
+        UIView.animate(withDuration: 2, delay: 0, options: [], animations: {
+            self.imageView.alpha = 1})
     }
 
 
